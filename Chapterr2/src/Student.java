314163eee1e0b82eaef9ed81f57515095945397d
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 14:17:17 +0900

    Studnet 클래스에 main() 함수 추가

--- Chapterr2/src/Student.java
+++ Chapterr2/src/Student.java
@@ -8,9 +8,11 @@ public class Student {
         return studentName;
     }
 
-    public void setStudentName(String name){
-        studentName = name;
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.studentName="최가을";
+
+        System.out.println(s1.studentName);
+        System.out.println(s1.showStudentInfo());
     }
-
-
 }
