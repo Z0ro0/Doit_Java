0e27992d26ef2ed7944323139303b8df03cafce4
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 15:08:56 +0900

    디폴트 생성자 직접 추가해서 오류 안 나게

--- Chapterr2/src/Person.java
+++ Chapterr2/src/Person.java
@@ -3,8 +3,9 @@ public class Person {
     float height;
     float weight;
     
-    public Person(){
+    public Person(){}//디폴트 생성자 직접 추가해 주어야 PersonTest에서 오류 안 남 -> 생성자가 하나도 없는 경우에만 디폴트 생성자를 제공하기 때문에
+    public Person(String pname){
         //자바 컴파일러가 자동으로 제공하는 디폴트 생성자, 따로 생성자를 만들지 않아도 자동으로 디폴트 생성자 만들어짐
-
+        name = pname;
     }
 }
