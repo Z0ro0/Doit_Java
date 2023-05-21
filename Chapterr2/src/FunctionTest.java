a5b518aa539fff330c6a6cccc36c65f61969d994
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 14:03:39 +0900

    합을 구하는 함수 구현하고 호출하기

---
+++ Chapterr2/src/FunctionTest.java
@@ -0,0 +1,13 @@
+public class FunctionTest {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = add(num1, num2);
+        System.out.println(sum);
+    }
+
+    public static int add(int n1, int n2){
+        return n1 + n2;
+    }
+}
