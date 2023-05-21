5eb4155b63756e147ecfb4f597ba40aa9413566d
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 14:20:29 +0900

    StudnetTest 실행 클래스 따로 만들기

---
+++ Chapterr2/src/StudnetTest.java
@@ -0,0 +1,9 @@
+public class StudnetTest {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.studentName="최가을";
+
+        System.out.println(s1.studentName);
+        System.out.println(s1.showStudentInfo());
+    }
+}
