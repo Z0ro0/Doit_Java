06b22cf786fe7d19c6c830e604eefc73e0bc5609
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 14:08:12 +0900

    학생 이름을 반환하는 메서드, 학생 이름을 부여하는 메서드

--- Chapterr2/src/Student.java
+++ Chapterr2/src/Student.java
@@ -4,7 +4,13 @@ public class Student {
     int grade;
     String address;
 
-    public void showStudentInfo(){
-            System.out.println(studentName+","+address);
+    public String showStudentInfo(){
+        return studentName;
     }
+
+    public void setStudentName(String name){
+        studentName = name;
+    }
+
+
 }
