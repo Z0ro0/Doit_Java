592ca9cd9d4f85ab58797c704084f810b812df60
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-21 15:06:23 +0900

    디폴트 생성자

--- Chapterr2/src/Person.java
+++ Chapterr2/src/Person.java
@@ -1,7 +1,10 @@
 public class Person {
     String name;
-    int height;
-    double weight;
-    char gender;
-    boolean married;
+    float height;
+    float weight;
+    
+    public Person(){
+        //자바 컴파일러가 자동으로 제공하는 디폴트 생성자, 따로 생성자를 만들지 않아도 자동으로 디폴트 생성자 만들어짐
+
+    }
 }
